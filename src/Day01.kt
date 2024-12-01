2b9da4e94e8b040b189e88e0e51109336af37bba
donald <donald@rdex.net>
2024-12-01 11:22:27 +0000

    Solution for Advent of Code Day 01 in Kotlin!  Cleaned up code to be more readable. Added skeletons for Days 2 and 3.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,49 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(listA: List<Int>, listB: List<Int> ): Int {
+        var listSum = 0
+        for (i in listA.indices) {
+            val listDiff = abs(listA[i] - listB[i])
+            listSum += listDiff
+        }
+
+        return listSum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(listA: List<Int>, listB: List<Int>): Int {
+        var listSum = 0
+        for (i in listA.indices) {
+            val occurrence = listB.count { it == listA[i] }
+            listSum += occurrence * listA[i]
+        }
+
+        return listSum
     }
+
+    fun inputToGroups(input: List<String>) : Pair<MutableList<Int>, MutableList<Int>> {
+        val groupOne = mutableListOf<Int>()
+        val groupTwo = mutableListOf<Int>()
+        input.forEach { it ->
+            val (a, b) = it.split("   ").map { it.toInt() }
+            groupOne.add(a)
+            groupTwo.add(b)
+        }
+        // Needed for part one for sure, not sure if will be needed for part two.
+        groupOne.sort()
+        groupTwo.sort()
+
+        return Pair(groupOne, groupTwo)
+    }
+
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val (testGroupA, testGroupB) = inputToGroups(testInput)
+    check(part1(testGroupA, testGroupB) == 11)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val (groupA, groupB) = inputToGroups(input)
+    println(part1(groupA, groupB))
+    println(part2(groupA, groupB))
 }
