5a56882d0096766f9f2d348de7a0ebbe7033c72f
donald <donald@rdex.net>
2024-12-03 07:41:48 +0000

    Day 3 part 1 completed using Regex'es

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,21 +1,28 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val multiplyPattern = "mul\\((?<a>[0-9]+),(?<b>[0-9]+)\\)".toRegex()
+
+        var mulSum = 0
+        for (line in input) {
+            val startRe = 0
+            multiplyPattern.findAll(line, startRe).forEach { match ->
+                mulSum += (match.groups["a"]?.value?.toInt() ?: 0) * (match.groups["b"]?.value?.toInt() ?: 0)
+            }
+        }
+
+        return mulSum
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("Day01_test")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 161)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day03")
     part1(input).println()
     part2(input).println()
 }
