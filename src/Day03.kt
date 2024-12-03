ce4ca502c7bba5e85c57d08fd3d08ae5d9a2bf52
donald <donald@rdex.net>
2024-12-03 09:05:56 +0000

    Day 3 part 2 WIP getting lost in regex hell

--- src/Day03.kt
+++ src/Day03.kt
@@ -14,7 +14,31 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val startPattern = "(?<beginning>.*)don't\\(\\)".toRegex()
+        val enablePattern = "do\\(\\)(?<enabled>.*)don't\\(\\)".toRegex()
+        val endPattern = "do\\(\\)(?<enabled>.*)$".toRegex()
+        var mulSum = 0
+
+        input.forEach { line ->
+            var startRe = 1
+            // lines start enabled, so only have to find where they stop
+            startPattern.find(line, startRe)?.let { match ->
+                startRe += match.groups["beginning"]?.range?.last?.plus(0) ?: 0
+                mulSum += part1(listOf("", match.groups["beginning"].toString()))
+            }
+            enablePattern.findAll(line, startRe).let { match ->
+                match.forEach { innerMatch ->
+                    startRe += innerMatch.groups["enabled"]?.range?.last?.plus(0) ?: 0
+                    mulSum += part1(listOf(innerMatch.groups["enabled"]?.value.toString()))
+                }
+            }
+            endPattern.find(line, startRe)?.let { match ->
+                mulSum += part1(listOf("", match.groups["enabled"].toString()))
+            }
+
+        }
+        println("returning: $mulSum")
+        return mulSum
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
@@ -24,5 +48,7 @@ fun main() {
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
     part1(input).println()
+
+    check(part2(testInput) == 48)
     part2(input).println()
 }
